09ac95fe914de6517e58fda9897236c0e29795b2
rkdhs <gaons120@gmail.com>
2024-08-22 21:26:23 +0900

    Python String

---
+++ 02.PythonBasic/03.String.py
@@ -0,0 +1,17 @@
+data = 'Hello World'
+print(data)
+
+# 이스케이프 시퀸스를 사용
+data = "Don't you know \"Python\"?"
+print(data)
+data[2] = 'a'  # 문자열은 불변(immutable)
+
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+a = "String"
+print(a * 3)
+
+a = "ABCDEF"
+print(a[2:4])
