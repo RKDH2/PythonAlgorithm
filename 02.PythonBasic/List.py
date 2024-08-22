31b248a0cd54d53564a4de113b6154c0340b052c
rkdhs <gaons120@gmail.com>
2024-08-22 20:43:45 +0900

    Python List

--- 02.PythonBasic/List.py
+++ 02.PythonBasic/List.py
@@ -29,11 +29,36 @@ m = 3
 array = [[0] * m for i in range(n)]
 print(array)
 
+a = [1, 2, 3]
+
 # append() : 리스트에 원소를 하나 삽입
+a.append(2)
+print(a)
 # sort() : 오름차순으로 정렬
+a.sort()
+print(a)
 # sort(reverse = True) : 내림차순으로 정렬
+a.sort(reverse=True)
+print(a)
 # insert(index, value) : 특정 인덱스에 요소를 삽입
+a.insert(2, 3)
+print(a)
 # remove() : 리스트에 첫 번째로 나타나는 값이 지정한 요소와 같으면 삭제
+a.remove(1)
+print(a)
 # count(value) : 리스트에 value가 몇 개 있는지 개수를 센다.
+a.count(3)
+print(a)
 # pop() : 마지막 요소 삭제
+a.pop()
+print(a)
 # clear() : 모든 요소 삭제
+a.clear()
+print(a)
+
+# remove_set에 들어있는 특정 값을 리스트에서 모두 제거하기
+a = [1, 2, 3, 4, 5, 5, 5]
+remove_set = {3, 5}
+
+result = [i for i in a if i not in remove_set]
+print(result)
