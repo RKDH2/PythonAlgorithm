07300216bc618fa79e9f0947645a3962ce47f48f
rkdhs <gaons120@gmail.com>
2024-08-22 18:53:24 +0900

    [CodeUp] Python Number

--- 02.PythonBasic/Number.py
+++ 02.PythonBasic/Number.py
@@ -1,3 +1,54 @@
 # 양의 정수
 a = 1000
 print(a)
+
+# 음의 정수
+a = -7
+print(a)
+
+# 0
+a = 0
+print(a)
+
+# 실수형
+a = 157.93
+print(a)
+
+# 음의 실수
+a = -1837.2
+print(a)
+
+# 소수부가 0일때 0을 생략
+a = 5.
+print(a)
+
+# 정수부가 0일 때 0을 생략
+a = -.7
+print(a)
+
+a = 1e9
+print(a)
+
+a = 75.25e1
+print(a)
+
+a = 3954e-3
+print(a)
+
+a = 0.3 + 0.6  # 미세한 값의 오차가 발생함
+print(a)
+
+a = round(a, 2)  # 세 번째 자리에서 반올림
+print(a)
+
+a = 7
+b = 3
+
+# 나누기 (실수형 반환)
+print(a / b)
+
+# 나누기 (나머지)
+print(a % b)
+
+# 나누기 (몫)
+print(a // b)
