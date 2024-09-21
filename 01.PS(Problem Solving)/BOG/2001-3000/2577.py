fe411be3fdc4324e9ec8bc5c2e31f7802a89383f
rkdhs <gaons120@gmail.com>
2024-09-21 22:57:51 +0900

    [BOJ] 2577

--- 01.PS(Problem Solving)/BOG/2001-3000/2577.py
+++ 01.PS(Problem Solving)/BOG/2001-3000/2577.py
@@ -5,3 +5,14 @@ total = list(str(a * b * c))
 
 for i in range(10):
     print(total.count(str(i)))
+
+# 다른 방법
+total = 1
+
+for _ in range(3):
+    total *= int(input())
+
+total = list(str(total))
+
+for i in range(10):
+    print(total.count(str(i)))
