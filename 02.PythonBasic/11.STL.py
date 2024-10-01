b2eb196ba88b719d602f4b1f1d84f7bdd7cedc37
rkdhs <gaons120@gmail.com>
2024-10-01 22:39:42 +0900

    Python STL

---
+++ 02.PythonBasic/11.STL.py
@@ -0,0 +1,9 @@
+result = sum([1, 2, 3, 4, 5])
+print(result)
+
+min_result = min([1, 2, 3, 4, 5])
+max_result = max([1, 2, 3, 4, 5])
+print(min_result, max_result)
+
+result = eval("(3+5)*7")
+print(result)
